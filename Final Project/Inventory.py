b75d738ba8f9b73946c823ea314070a987c3f0f6
allmonjj <allmonjj@miamioh.edu>
2024-12-02 12:58:34 -0500

    Project Initiation
    Began setting up the "foundation" of the game.

---
+++ Final Project/Inventory.py
@@ -0,0 +1,19 @@
+class Inventory():
+    def __init__(self):
+        self.items = {}
+
+    def add_Item(self, item, quantity):
+        if item not in self.items:
+            self.items[item] = 0
+        self.items[item] += quantity
+
+    def remove_Item(self, item):
+        if item in self.items and self.items[item] > 0:
+            self.items[item] -= 1
+            if self.items[item] == 0:
+                del self.items[item]
+        else:
+            print(f"Don't have enough {item.name}'s!")
+
+    def get_Inventory(self):
+        return self.items
---
+++ Final Project/Main.py
@@ -0,0 +1,10 @@
+import Player
+import Inventory
+import Item
+
+
+def main():
+    player = Player.Player(100,0, Inventory.Inventory())
+
+if __name__ == "__main__":
+    main()
